612f3e77f6ba642a6f2df153aa76f5ceada6370c
joliathan souza <coelhodesouzajoliathan1@gmail.com>
2021-09-26 14:46:48 -0300

    exercício

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace ol__mundo
+namespace olá_mundo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Olá Usuário, me chamo joliathan!");
+            Console.WriteLine("Esse é me meu primeiro programa em C#, ainda bem simples");
+            Console.WriteLine("Mas futuramente começarei a desenvolver progrmas mais elaborados.");
+            Console.WriteLine(":)");
+       }
     }
-}
+    }
